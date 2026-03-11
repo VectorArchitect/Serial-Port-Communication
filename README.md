c106a0b3d4f769ed179822f6c52ecfd0b8bfdc9c
SAVINAV <savinavsharma2005@gmail.com>
2026-03-11 23:49:03 +0530

    Added program for recieving/sending data from an embedded system adn string it in text files.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Serial-Port-Communication
+When we connect a serial port to a computer machine it transfers the signal data to it
+
+This program connacts with the hardware and recieves the data and stores in a file
--- main.py
+++ main.py
@@ -13,11 +13,13 @@ try:
         print(f"Connected to {ser.port}")
         ser.write(b'Hello')
         print("Data sent: Hello")
+        open('data_sent.txt', 'w').write('Hello')               #This writes the data into data_sent.txt file
         time.sleep(0.1)
 
         if ser.in_waiting > 0:
             data=ser.readline().decode('utf-16').rstrip()       #This reads the data from ser and decodes it via using utf-16 format and strips the extra space of empty line at last
             print(f"Data received : {data}")
+            open('data_recieved.txt', 'w').write(data)     #This writes the data into data_recieved.txt file
 except Exception as e:
     print(f"Error: {e}")
 finally:
